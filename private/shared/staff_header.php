9b711c024e4c6e65a410b2bdc8ca41671691600c
Ehbraheem <ehbraheem@gmail.com>
2017-09-08 11:34:39 -0700

    Start a Database-Driven Project

---
+++ private/shared/staff_header.php
@@ -0,0 +1,24 @@
+<?php 
+	if (!isset($page_title)) {
+		$page_title = 'Staff Area';
+	}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GBI - <?php echo $page_title; ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" media="all" href="../stylesheet/staff.css" />
+
+</head>
+<body>
+
+	<header>
+		<h1>GBI Staff Area</h1>
+	</header>
+
+	<navigation>
+		<ul>
+			<li><a href="index.php">Menu</a></li>
+		</ul>
+	</navigation>
---
+++ public/staff/index.php
@@ -0,0 +1,13 @@
+<?php $PRIVATE_PATH='../../private/' ?>
+<?php $SHARED_PATH=$PRIVATE_PATH.'shared/' ?>
+
+<?php $page_title = "Staff Area"; ?>
+
+<?php require_once $PRIVATE_PATH.'initialize.php'; ?>
+
+<?php include $SHARED_PATH.'staff_header.php'; ?>
+
+<div id="content">
+</div>
+
+<?php include $SHARED_PATH.'staff_footer.php'; ?>
